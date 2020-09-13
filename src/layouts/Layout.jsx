fbd92976dcafbc0f54b48b461a5d6f97ebf8e3d2
Sam Killermann <sK@hues.xyz>
2020-09-13 14:48:19 -0500

    idk

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,6 +2,15 @@ import React from "react"
 import { ModalRoutingContext } from "gatsby-plugin-modal-routing"
 import "../styles/style.css"
 
+import Rukou from '..fonts/rukou.woff2'
+const globals = css`
+  @font-face {
+    font-family: 'Rukou';
+    src: url(${Rukou}) format('woff2');
+    font-weight: 500;
+  }
+`
+
 import { LayoutFull } from "./LayoutFull"
 import { LayoutModal } from "./LayoutModal"
 
